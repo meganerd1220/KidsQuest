5103e9bbc901f3c675b78559531c145367a43d8d
meganerd1220 <123340494+meganerd1220@users.noreply.github.com>
2024-03-14 16:31:16 -0500

    updated choresCollection

--- KidsQuest-master/model/database.js
+++ KidsQuest-master/model/database.js
@@ -55,11 +55,9 @@ export const sendNewCredentials = async (name, lastn, email, username, password)
 
 export const sendNewChores = async (chore) => {}
 const firestore = getFirestore(app);
-const usersCollection = collection(firestore, 'Chores');
+const choresCollection = collection(firestore, 'Chores');
 
-firestore()
-  .collection('Chores')
-  .add({
+choresCollection.add({
     chore: chore,
   })
   .then(() => {
