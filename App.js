72f8b957a47a432a27832fac2e123ebec3d387bd
robinhrdz <103783051+robinhrdz@users.noreply.github.com>
2024-04-09 15:29:21 -0500

    deletec StyleSheet From App.js

--- App.js
+++ App.js
@@ -39,20 +39,6 @@ function App() {
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 100,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-});
+
 
 export default App;
